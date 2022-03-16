1123fb4df29031f51c91f89c6dacc4e15bccfce0
SSunan <85893362+SSunan@users.noreply.github.com>
2022-03-16 17:26:11 -0400

    Delete DeployToAzure_CLI.ps1

--- DeployToAzure_CLI.ps1
+++
@@ -1,6 +0,0 @@
-#Create a new resource group named 'MyGeotab1' and location in 'canadacentral'
-az group create --name MyGeotab1 --location canadacentral
-
-#Start a deployment at resource group 'MyGeotab1' from a template file 'template.json' 
-#using a parameter file 'template_parameters.json'
-az deployment group create -g MyGeotab1 --template-file template.json --parameters template_parameters.json --verbose
